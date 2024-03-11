683dee9a0b0ce5b3a86a5126ef53603ff9b9a8f0
Turjoy Saha <sahaturjoy@gmail.com>
2024-03-12 03:40:13 +0530

    actions simplified

--- src/store/actions/todo.tsx
+++ src/store/actions/todo.tsx
@@ -2,37 +2,28 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import {Task, TaskResp} from '../../../types';
 import BaseURL from '../../constants/baseUrl';
 
-type ResolveType<T> = (value: T | PromiseLike<T>) => void;
-type RejectType = (reason: string) => void;
-
 export const fetchTodos = createAsyncThunk('todo/fetch', async () => {
-  return new Promise((resolve: ResolveType<TaskResp[]>, reject: RejectType) => {
-    fetch(`${BaseURL}/todos.json`)
-      .then(response => {
-        if (!response.ok) {
-          reject(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        let resData: any = data;
-        let arr: TaskResp[] = [];
+  try {
+    const response = await fetch(`${BaseURL}/todos.json`);
 
-        for (let key in resData) {
-          console.log(key, resData[key]);
-          arr.push({
-            id: key,
-            description: resData[key].description,
-            pending: resData[key].pending,
-          });
-        }
+    if (!response.ok) {
+      return Promise.reject(`HTTP error! Status: ${response.status}`);
+    }
+    const resData = await response.json();
+    const arr: TaskResp[] = [];
 
-        resolve(arr);
-      })
-      .catch(error => {
-        reject(error);
+    for (const key in resData) {
+      arr.push({
+        id: key,
+        description: resData[key].description,
+        pending: resData[key].pending,
       });
-  });
+    }
+
+    return Promise.resolve(arr);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 });
 
 export const addTodo = createAsyncThunk('todo/add', async (task: string) => {
@@ -41,28 +32,23 @@ export const addTodo = createAsyncThunk('todo/add', async (task: string) => {
     pending: true,
   };
 
-  return new Promise((resolve: ResolveType<TaskResp>, reject: RejectType) => {
-    fetch(`${BaseURL}/todos.json`, {
+  try {
+    const response = await fetch(`${BaseURL}/todos.json`, {
       method: 'POST',
       body: JSON.stringify({...todo}),
-    })
-      .then(response => {
-        if (!response.ok) {
-          reject(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        let resData: any = data;
-        resolve({
-          ...todo,
-          id: `${resData.name}`,
-        });
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
+    });
+    if (!response.ok) {
+      return Promise.reject(`HTTP error! Status: ${response.status}`);
+    }
+    const resData = await response.json();
+
+    return Promise.resolve({
+      ...todo,
+      id: `${resData.name}`,
+    });
+  } catch (error) {
+    return Promise.reject(error);
+  }
 });
 
 interface updateDetailsParams {
@@ -73,34 +59,28 @@ interface updateDetailsParams {
 export const updateTodoDetails = createAsyncThunk(
   'todo/updateDetails',
   async ({id, description}: updateDetailsParams) => {
-    return new Promise(
-      (resolve: ResolveType<updateDetailsParams>, reject: RejectType) => {
-        fetch(`${BaseURL}/todos/${id}.json`, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({description}),
-        })
-          .then(response => {
-            if (!response.ok) {
-              reject(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-          })
-          .then(data => {
-            let resData: any = data;
+    try {
+      const response = await fetch(`${BaseURL}/todos/${id}.json`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({description}),
+      });
 
-            resolve({
-              description,
-              id,
-            });
-          })
-          .catch(error => {
-            reject(error);
-          });
-      },
-    );
+      if (!response.ok) {
+        return Promise.reject(`HTTP error! Status: ${response.status}`);
+      }
+
+      await response.json();
+
+      return Promise.resolve({
+        description,
+        id,
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
   },
 );
 
@@ -112,61 +92,48 @@ interface updateStatusParams {
 export const updateTodoStatus = createAsyncThunk(
   'todo/updateStatus',
   async ({id, pending}: updateStatusParams) => {
-    return new Promise((resolve: ResolveType<string>, reject: RejectType) => {
-      fetch(`${BaseURL}/todos/${id}.json`, {
+    try {
+      const response = await fetch(`${BaseURL}/todos/${id}.json`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({pending: !pending}),
-      })
-        .then(response => {
-          if (!response.ok) {
-            reject(`HTTP error! Status: ${response.status}`);
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then(data => {
-          resolve(id);
-        })
-        .catch(error => {
-          reject(error);
-        });
-    });
+      });
+
+      if (!response.ok) {
+        return Promise.reject(`HTTP error! Status: ${response.status}`);
+      }
+
+      await response.json();
+
+      return Promise.resolve(id);
+    } catch (e) {
+      return Promise.reject(e);
+    }
   },
 );
 
 export const deleteTodo = createAsyncThunk(
   'todo/delete',
   async (id: string) => {
-    const response = await fetch(`${BaseURL}/todos/${id}.json`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    return new Promise((resolve: ResolveType<string>, reject: RejectType) => {
-      fetch(`${BaseURL}/todos/${id}.json`, {
+    try {
+      const response = await fetch(`${BaseURL}/todos/${id}.json`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
         },
-      })
-        .then(response => {
-          if (!response.ok) {
-            reject(`HTTP error! Status: ${response.status}`);
-          } else {
-            return response.json();
-          }
-        })
-        .then(data => {
-          resolve(id);
-        })
-        .catch(error => {
-          reject(error);
-        });
-    });
+      });
+
+      if (!response.ok) {
+        return Promise.reject(`HTTP error! Status: ${response.status}`);
+      }
+
+      await response.json();
+
+      return Promise.resolve(id);
+    } catch (error) {
+      return Promise.reject(error);
+    }
   },
 );
